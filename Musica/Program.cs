7c134514210c7fb919f5f6db2aa06bac814a2bc8
MWLeonel <wmleonel6@gmail.com>
2020-09-05 10:43:13 -0300

    Letra da Musica

---
+++ Musica/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //<---Inicio(Titulo & Cantor)--->//
+            Console.Write("Titulo: ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Vou Para o Alvo (feat. André e Felipe)");
+            Console.Write("\n");
+            Console.ResetColor();
+
+            Console.Write("Cantor: ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("DJ PV");
+            Console.Write("\n");
+            Console.ResetColor();
+            //<---Fim(Titulo & Cantor)--->//
+
+            Console.WriteLine("Posso ouvir a sua voz nessa escuridão");
+            Console.WriteLine("Nos desafios dessa vida vou na contramão");
+            Console.WriteLine("Contigo sei que posso andar sobre as águas");
+            Console.WriteLine("Não vou me afundar");
+            Console.Write("\n");
+
+            Console.WriteLine("E quanto mais eu me aproximo mais me sinto assim");
+            Console.WriteLine("Tão tranquilo porque sinto Deus cuidar de mim");
+            Console.WriteLine("A tempestade turbulenta não me atrapalha");
+            Console.WriteLine("Estou seguro");
+            Console.Write("\n");
+
+            //<---Inicio(Refão 1)--->//
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("E vou para o alvo eu vou");
+            Console.WriteLine("Nada me impedirá");
+            Console.WriteLine("Sou mais que vencedor");
+            Console.WriteLine("Comigo Deus está");
+            Console.Write("\n");
+
+            Console.WriteLine("Vou para o alvo eu vou");
+            Console.WriteLine("Nada me impedirá");
+            Console.WriteLine("Sou mais que vencedor");
+            Console.WriteLine("Comigo Deus está");
+            Console.Write("\n");
+
+            Console.WriteLine("Vou para o alvo eu vou");
+            Console.WriteLine("Sou mais que vencedor...");
+            Console.ResetColor();
+            Console.Write("\n");
+            //<---Fim(Refrão 1)--->//
+
+            Console.WriteLine("Posso ouvir a sua voz nessa escuridão");
+            Console.WriteLine("Nos desafios dessa vida vou na contramão");
+            Console.WriteLine("Contigo sei que posso andar sobre as águas");
+            Console.WriteLine("Não vou me afundar");
+            Console.Write("\n");
+
+            Console.WriteLine("E quanto mais eu me aproximo mais me sinto assim");
+            Console.WriteLine("Tão tranquilo porque sinto Deus cuidar de mim");
+            Console.WriteLine("A tempestade turbulenta não me atrapalha");
+            Console.WriteLine("Estou seguro");
+            Console.Write("\n");
+
+            //<---Inicio(Refão 2)--->//
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("E vou para o alvo eu vou");
+            Console.WriteLine("Nada me impedirá");
+            Console.WriteLine("Sou mais que vencedor");
+            Console.WriteLine("Comigo Deus está");
+            Console.Write("\n");
+
+            Console.WriteLine("Vou para o alvo eu vou");
+            Console.WriteLine("Nada me impedirá");
+            Console.WriteLine("Sou mais que vencedor");
+            Console.WriteLine("Comigo Deus está");
+            Console.Write("\n");
+
+            Console.WriteLine("Vou para o alvo eu vou");
+            Console.WriteLine("Sou mais que vencedor...");
+            Console.ResetColor();
+            Console.Write("\n");
+            //<---Fim(Refrão 2)--->//
+
+            Console.WriteLine("A tua mão que me sustenta");
+            Console.WriteLine("A tua voz me orienta");
+            Console.WriteLine("Para onde eu vou");
+            Console.WriteLine("Para onde eu vou");
+            Console.WriteLine("Tu és senhor o meu caminho");
+            Console.WriteLine("Sem ti não sei andar sozinho");
+            Console.WriteLine("Tu me Guiará");
+            Console.WriteLine("Tu me Guiará");
+            Console.WriteLine("Tu me Guiará");
+            Console.Write("\n");
+
+            //<---Inicio(Refrão 3)--->//
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Vou para o alvo eu vou...");
+            Console.Write("\n");
+
+            Console.WriteLine("Sou mais que vencedor...");
+            Console.Write("\n");
+
+            Console.WriteLine("Vou para o alvo eu vou");
+            Console.WriteLine("Nada me impedirá");
+            Console.WriteLine("Sou mais que vencedor");
+            Console.WriteLine("Comigo Deus está");
+            Console.Write("\n");
+
+            Console.WriteLine("Vou para o alvo eu vou");
+            Console.WriteLine("Nada me impedirá");
+            Console.WriteLine("Sou mais que vencedor");
+            Console.WriteLine("Comigo Deus está");
+            Console.Write("\n");
+            Console.ResetColor();
+            //<---Fim(Refrão 3)--->//
+
+        }
+    }
+}
